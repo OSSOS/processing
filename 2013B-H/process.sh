591ffd4576ee963d022ce0897e9e8a5064f814cc
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-06-03 16:57:53 -0700

    working version

--- 2013B-H/process.sh
+++ 2013B-H/process.sh
@@ -9,7 +9,7 @@ export rmin=0.5
 export ang=-90
 export width=90
 export field=$4
-
+export force=
 basedir=`pwd`
 
 for ccd in {0..35} 
@@ -17,35 +17,35 @@ for ccd in {0..35}
   mkdir ${ccd}
   cd ${ccd}
   ## First do the search images
-  mkpsf.py $1 $2 $3 -v --ccd ${ccd}
-  step1.py $1 $2 $3 -v --ccd ${ccd}
-  step2.py $1 $2 $3 -v --ccd ${ccd}
-  step3.py $1 $2 $3 -v --rate_min 0.5 --angle -90 --width 90  --ccd ${ccd}
+  mkpsf.py $1 $2 $3 -v --ccd ${ccd} ${force}
+  step1.py $1 $2 $3 -v --ccd ${ccd} ${force}
+  step2.py $1 $2 $3 -v --ccd ${ccd} ${force}
+  step3.py $1 $2 $3 -v --rate_min 0.5 --angle -90 --width 90  --ccd ${ccd} ${force}
   echo "Running combine.py"
-  combine.py $1 -v --measure3 ${MEASURE3} --field ${field}  --ccd ${ccd}
+  combine.py $1 -v --measure3 ${MEASURE3} --field ${field}  --ccd ${ccd} ${force}
 
   # First scramble the images.
-  scramble.py $1 $2 $3 --ccd $ccd -v --dbimages ${DBIMAGES} 
+  scramble.py $1 $2 $3 --ccd $ccd -v --dbimages ${DBIMAGES}  ${force}
 
   # now run the standard pipeline on the scramble images..
-  mkpsf.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES} 
-  step1.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES} 
-  step2.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES} 
-  step3.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES} --rate_min ${rmin} --rate_max ${rmax} --angle ${ang} --width ${width}
-  combine.py $1 --ccd $ccd --type s -v --dbimages ${DBIMAGES} --measure3 ${MEASURE3} --field ${field}
+  mkpsf.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES}  ${force}
+  step1.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES}  ${force}
+  step2.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES}  ${force}
+  step3.py $1 $2 $3 --ccd $ccd -v --type s --dbimages ${DBIMAGES} --rate_min ${rmin} --rate_max ${rmax} --angle ${ang} --width ${width} ${force}
+  combine.py $1 --ccd $ccd --type s -v --dbimages ${DBIMAGES} --measure3 ${MEASURE3} --field ${field} ${force}
 
   # Now plant artificial sources.
-  plant.py $1 $2 $3 --ccd $ccd -v --dbimages ${DBIMAGES} --type s --rmin ${rmin} --rmax ${rmax} --ang ${ang} --width ${width} 
+  plant.py $1 $2 $3 --ccd $ccd -v --dbimages ${DBIMAGES} --type s --rmin ${rmin} --rmax ${rmax} --ang ${ang} --width ${width}  ${force}
 
   # Now run the standard pipeline on the artificial sources.
-  mkpsf.py $1 $2 $3 --ccd $ccd --fk -v --type s --dbimages ${DBIMAGES} --type s
-  step1.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} 
-  step2.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} 
-  step3.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} --rate_min ${rmin} --rate_max ${rmax} --angle ${ang} --width ${width} 
-  combine.py $1 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} --measure3 ${MEASURE3}
+  mkpsf.py $1 $2 $3 --ccd $ccd --fk -v --type s --dbimages ${DBIMAGES} --type s ${force}
+  step1.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES}  ${force}
+  step2.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES}  ${force}
+  step3.py $1 $2 $3 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} --rate_min ${rmin} --rate_max ${rmax} --angle ${ang} --width ${width}  ${force}
+  combine.py $1 --ccd $ccd --fk --type s -v --dbimages ${DBIMAGES} --measure3 ${MEASURE3} ${force}
 
   # compute the variation in magnitudes from planeted images
-  astrom_mag_check.py fk_${field}_s${ccd}.measure3.cands.astrom  --dbimages ${DBIMAGES}
-  vcp fk_${field}_s${ccd}.measure3.cands.match ${MEASURE3}
+  astrom_mag_check.py fk$1s*${ccd}.measure3.cands.astrom  --dbimages ${DBIMAGES} 
+  vcp fk$1s*${ccd}.measure3.cands.match ${MEASURE3} 
   cd ${basedir}
 done
